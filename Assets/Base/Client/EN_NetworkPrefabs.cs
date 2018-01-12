6c32394667974cfbeaa930b70824da1361725878
Stureplan <linus.ax@gmail.com>
2018-01-12 18:43:13 +0100

    ScriptableObject introduced. Array needs to be built on build and on editor play

---
+++ Assets/Base/Client/EN_NetworkPrefabs.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NS_GamePathData", menuName = "Neon Smoke/Create Game Path", order = 52)]
+public class EN_NetworkPrefabs : ScriptableObject
+{
+    public GameObject[] networkPrefabs;
+}
---
+++ Assets/Editor/EN_NetworkPrefabBuilder.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EN_NetworkPrefabBuilder : Editor
+{
+
+}
